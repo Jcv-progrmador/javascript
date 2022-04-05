31fa9b9eab5f972718f2b5e2f516b884c38f7564
Jcv programador <102539132+Jcv-progrmador@users.noreply.github.com>
2022-04-05 11:17:49 -0300

    adicionar css

---
+++ aula11/js/script.js
@@ -0,0 +1,5 @@
+var title = document.querySelector("#title")
+title.style.color = 'red';
+title.style.backgroundColor = 'yellow';
+var subtitle = document.querySelector('.subtitle')
+subtitle.style.cssText = "color: blue; background-color: pink; font-size: 50px;"
