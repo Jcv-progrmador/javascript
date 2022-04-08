32b457c9c4f6eda991cc42571dc629996a3695d4
Jcv programador <102539132+Jcv-progrmador@users.noreply.github.com>
2022-04-08 10:55:31 -0300

    callback

---
+++ aula14/js/script.js
@@ -0,0 +1,16 @@
+function exibir(num){
+    console.log(num)
+}
+
+function soma(a, b, callback){
+    var op = a + b
+    callback(op)
+}
+
+function multiplicacao(a ,b, cb){
+    var op = a * b
+    cb(op)
+}
+
+soma(2 , 2 , exibir)
+multiplicacao(2,2, exibir)
