90901a9074437b9a6f3795f748b2b55122d81265
Jcv programador <102539132+Jcv-progrmador@users.noreply.github.com>
2022-04-08 12:01:15 -0300

    setTimeout

---
+++ aula15/js/script.js
@@ -0,0 +1,9 @@
+console.log('Antes do codigo')
+setTimeout(function(){
+    console.log("testando o setTimeOut")
+}, 2000)
+console.log('Depois do codigo')
+
+setInterval(function(){
+    console.log('Testando setInterval')
+}, 1000)
