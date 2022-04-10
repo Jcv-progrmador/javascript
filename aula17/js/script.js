f280c04a0f6053ea51afb50fa8e2b620a73c61b6
Jcv programador <102539132+Jcv-progrmador@users.noreply.github.com>
2022-04-10 14:18:07 -0300

    onload

---
+++ aula17/js/script.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    console.log('Carregou o Dom')
+    var title = document.querySelector("#title")
+    console.log(title)
+}
+
+console.log('Carregou o Js')
+
+var title = document.querySelector("#title")
+console.log(title)
