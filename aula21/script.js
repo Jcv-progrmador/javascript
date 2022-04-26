a997e7e119792603113fd70590d4227a2dfc6e1a
Jcv programador <102539132+Jcv-progrmador@users.noreply.github.com>
2022-04-26 13:01:54 -0300

    objeto

--- aula21/script.js
+++ aula21/script.js
@@ -3,10 +3,17 @@ let pessoa = {
     idade: 29,
     falar: function(){
         console.log("Ola , tudo bem?")
+    },
+
+    soma: function(a, b){
+        return a + b;
     }
 };
 
 console.log(pessoa.nome)
 
-pessoa.falar;
+pessoa.falar();
+
+var soma = pessoa.soma(2, 2);
+console.log(soma)
 
